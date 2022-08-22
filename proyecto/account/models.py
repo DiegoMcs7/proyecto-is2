71b00b7d965f3b8f712edb291d1b019f4b4dd3b1
DENIS ESQUIVEL <denis03esquivel@gmail.com>
2022-08-22 13:12:28 -0400

    Modificaciones a la documentacion

--- proyecto/account/models.py
+++ proyecto/account/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+'''
+     Vistas
+     fecha: 20/8/2022
 
+    Posteriormente en el modelo models.py se agregarán las clases que corresponden a tablas de la base de datos
+
+
+    Para Importar desde la carpeta donde creamos las aplicaciones la clase a llamar.
+        Dentro de la clase donde estamos modificando incluir el nombre de la clase que llamaremos.
+        Creamos una nueva clase a la que se le aplicara la función de enlace.
+        Incluimos dentro de la clase donde nos encontramos la nueva clase creada junto con la relación que deseamos que tenga.
+'''
+
+from django.db import models
 # Create your models here.
