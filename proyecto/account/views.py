9873c1b0283f3ab8f39220d1bb206bc47b539e67
DENIS ESQUIVEL <denis03esquivel@gmail.com>
2022-08-22 12:59:36 -0400

    documentacion

--- proyecto/account/views.py
+++ proyecto/account/views.py
@@ -7,8 +7,11 @@ from django.contrib.auth.decorators import login_required
 @login_required()
 def tablero(request):
     '''
-    REDIRECCIONAMIENTO A LA PANTALLA DE MENU
-    :param request:
-    :return:
+        Vistas
+        fecha: 20/8/2022
+
+        Una función de visualización es una función de Python que toma una solicitud web y devuelve una respuesta web .
+               Esta vista es la encargada de llamar al archivo tablero.html con el fin de mostrar en pantalla el menu
+               luego de la peticion correspondiente
     '''
     return render(request,'account/tablero.html', {'section':'tablero'})
